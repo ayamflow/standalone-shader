bdf8c90ba67c5b2cec31e431a6d0604b3479575e
Nik Rowell <nik@nikrowell.com>
2019-01-28 09:53:37 -0700

    change onload implementation to use self... becaues it looks cleaner

--- texture.js
+++ texture.js
@@ -32,11 +32,11 @@ export default class Texture {
             gl.bindTexture(gl.TEXTURE_2D, texture)
             gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, new Uint8Array([0, 0, 0, 255]))
 
-            const onload = () => {
+            const self = this
+            image.addEventListener('onload', function onload() {
                 image.removeEventListener('load', onload)
-                this.update()
-            }
-            image.addEventListener('load', onload);
+                self.update()
+            })
         }
     }
 
